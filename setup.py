6a95eaebc8374410f1a437a893598df6cc0fce12
cmrfrd <alexanderjcomerford@gmail.com>
2020-03-09 01:17:28 -0400

    add setup config and setup

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-    Setup file for snakestring.
-    Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 3.2.3.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
+"""Setup file for snakestring.
 """
 import sys
 
